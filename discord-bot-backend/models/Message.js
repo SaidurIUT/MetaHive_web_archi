5b2114516172940ccdd1e9c174ec8a8b1ffa979c
NafisFuadShahid <fuadnafis9191@gmail.com>
2025-10-29 18:59:53 +0600

    new schema added for discord service

--- discord-bot-backend/models/Message.js
+++ discord-bot-backend/models/Message.js
@@ -5,8 +5,11 @@ const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
   username: { type: String, required: true },
   avatar: { type: String },
+  content: { type: String, required: true },
   timestamp: { type: Date, required: true },
   channel: { type: String, required: true },
+  channelId: { type: String, required: true },
+  guild: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Message", messageSchema);
